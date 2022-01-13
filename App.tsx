65e818d126173b0a9edff43723c569548b38c470
Nazmul Hussain <nazmul.hussain@streamstech.com>
2022-01-13 14:37:56 +0600

    redux try

--- App.tsx
+++ App.tsx
@@ -3,12 +3,16 @@ import * as eva from '@eva-design/eva';
 import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
 import {EvaIconsPack} from '@ui-kitten/eva-icons';
 import {AppNavigator} from './src/navigations/app.navigator.component';
+import { Provider } from 'react-redux';
+import { store } from './src/store/store';
 
 export default () => (
   <>
     <IconRegistry icons={EvaIconsPack} />
     <ApplicationProvider {...eva} theme={eva.light}>
-      <AppNavigator />
+      <Provider store={store}>
+        <AppNavigator />
+      </Provider>
     </ApplicationProvider>
   </>
 );
--- src/screens/share.screen.component.tsx
+++ src/screens/share.screen.component.tsx
@@ -1,9 +1,70 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList, PermissionsAndroid, Platform, SafeAreaView, StyleSheet, TextInput, View } from 'react-native';
 import { Divider, Layout, Text, TopNavigation } from '@ui-kitten/components';
 import { renderBackAction, renderRightActions } from '../core/renderer.component';
+import Contacts from 'react-native-contacts';
+import ContactListItem from '../components/contact-list-item.component'
 
 export const ShareScreen = ({ navigation }: any) => {
+  const [contacts, setContacts] = useState([]);
+  const [inMemContacts, setInMemContacts] = useState([]);
+
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.READ_CONTACTS, {
+          title: 'Contacts',
+          message: 'This app would like to view your contacts.',
+        }).then(() => {
+          loadContacts();
+        }
+      );
+    } else {
+      loadContacts();
+    }
+  }, []);
+
+  const loadContacts = () => {
+    Contacts.getAll().then((contacts) => {
+      contacts.sort(
+        (a, b) => 
+          a.givenName.toLowerCase() > b.givenName.toLowerCase(),
+      );
+      setContacts(contacts);
+      setInMemContacts(contacts);
+      console.log('contacts -> ', contacts);
+      // if (err === 'denied') {
+      //   alert('Permission to access contacts was denied');
+      //   console.warn('Permission to access contacts was denied');
+      // } else {
+      //   console.log('contacts', contacts);
+      // }
+    });
+  };
+  const keyExtractor = (item, idx) => {
+    return item?.recordID?.toString() || idx.toString();
+  };
+  const renderItem = ({item, index}) => {
+    return <ContactListItem contact={item} />;
+  };
+
+  const searchContacts = srcText => {
+    const filteredContacts = inMemContacts.filter(contact => {
+      let contactLowercase = (
+        contact.givenName +
+        ' ' +
+        contact.familyName +
+        ' ' + 
+        contact?.phoneNumbers[0]?.number + '' + contact?.phoneNumbers[1]?.number
+      ).toLowerCase();
+
+      let searchTermLowercase = srcText.toLowerCase();
+
+      return contactLowercase.indexOf(searchTermLowercase) > -1;
+    });
+    setContacts(filteredContacts);
+  };
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <TopNavigation
@@ -14,9 +75,38 @@ export const ShareScreen = ({ navigation }: any) => {
         accessoryRight={renderRightActions}
       />
       <Divider/>
-      <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Share this app with friends</Text>
-      </Layout>
+      <View style={styles.container}>
+        <TextInput
+          onChangeText={searchContacts}
+          placeholder="Search"
+          style={styles.searchBar}
+        />
+        <FlatList
+          data={contacts}
+          renderItem={renderItem}
+          keyExtractor={keyExtractor}
+          style={styles.list}
+        />
+      </View>
+      
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    backgroundColor: '#4591ed',
+    color: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    fontSize: 20,
+  },
+  searchBar: {
+    backgroundColor: '#f0eded',
+    paddingHorizontal: 30,
+    paddingVertical: Platform.OS === 'android' ? undefined : 15,
+  },
+});
